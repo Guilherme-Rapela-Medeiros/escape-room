593b1705234263e3f78be8c4fcdb9905d726b675
Guilherme-Rapela-Medeiros <guilhermerapela9@gmail.com>
2025-11-07 20:15:15 -0300

    adição de código para movimento vertical do jogador

--- inputs.h
+++ inputs.h
@@ -5,14 +5,14 @@
 #include "structs.h"
 #include <ctype.h>
 
-void inputs_jogador_movimento(rectangle *jogador, int largura_tela, int velocidade_jogador, fases.obstaculo *obstaculos){
+void inputs_jogador_movimento(Rectangle *jogador, int largura_tela, int tamanho_tela, int velocidade_jogador, obstaculo *obstaculos){
     if (IsKeyDown(KEY_RIGHT)){
         jogador->x += velocidade_jogador;  // posição horizontal do jogador
         if (jogador->x + jogador->width > largura_tela) {
             jogador->x = largura_tela - jogador->width;
         }
     }
-}
+
     if (IsKeyDown(KEY_LEFT)){
         jogador->x -= velocidade_jogador;
         if (jogador->x < 0) {
@@ -20,10 +20,21 @@ void inputs_jogador_movimento(rectangle *jogador, int largura_tela, int velocida
         }
     }
 
-    if ((IsKeyPressed(KEY_SPACE) ) || (IsKeyDown(KEY_UP))){}
+    if ((IsKeyPressed(KEY_SPACE) ) || (IsKeyDown(KEY_UP))){
+        jogador->y -= velocidade_jogador;
+        if (jogador->y < 0) {
+            jogador->y = 0;
+        }
+    }
        
-    if(IsKeyDown(KEY_DOWN)){}
+    if(IsKeyDown(KEY_DOWN)){
+        jogador->y += velocidade_jogador;
+        if (jogador->y + jogador->height > tamanho_tela) {
+            jogador->y = tamanho_tela - jogador->height;
+        }
+    }
+}
 
-
+#endif
 
 //criado o arquivo dos inputs
