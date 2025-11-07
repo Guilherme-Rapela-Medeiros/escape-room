56d26c8d74a3db88f0192479c7dc34c9d5b8115f
Guilherme-Rapela-Medeiros <guilhermerapela9@gmail.com>
2025-11-07 19:15:57 -0300

    começo da implementação da funções de movimento do jogador

--- inputs.h
+++ inputs.h
@@ -1,1 +1,19 @@
+#ifndef INPUTS_H
+#define INPUTS_H
+
+#include "raylib.h"
+#include "structs.h"
+#include <ctype.h>
+
+void inputs_jogador_movimento(rectangle *jogador, int largura_tela, int velocidade_jogador, fases.obstaculo *obstaculos){
+    if (IsKeyDown(KEY_RIGHT)){}
+
+    if (IsKeyDown(KEY_LEFT)){}
+
+    if ((IsKeyPressed(KEY_SPACE) ) || (IsKeyDown(KEY_UP))){}
+
+    if(IsKeyDown(KEY_DOWN)){}
+
+}
+
 //criado o arquivo dos inputs
