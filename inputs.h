b68bd56162ddb36228720c716d852192c11782af
rafaelpborba1 <rpb@cesar.school>
2025-11-07 19:52:48 -0300

    adição da movimentação da esquerda e direita

--- inputs.h
+++ inputs.h
@@ -6,14 +6,24 @@
 #include <ctype.h>
 
 void inputs_jogador_movimento(rectangle *jogador, int largura_tela, int velocidade_jogador, fases.obstaculo *obstaculos){
-    if (IsKeyDown(KEY_RIGHT)){}
-
-    if (IsKeyDown(KEY_LEFT)){}
+    if (IsKeyDown(KEY_RIGHT)){
+        jogador->x += velocidade_jogador;  // posição horizontal do jogador
+        if (jogador->x + jogador->width > largura_tela) {
+            jogador->x = largura_tela - jogador->width;
+        }
+    }
+}
+    if (IsKeyDown(KEY_LEFT)){
+        jogador->x -= velocidade_jogador;
+        if (jogador->x < 0) {
+            jogador->x = 0;
+        }
+    }
 
     if ((IsKeyPressed(KEY_SPACE) ) || (IsKeyDown(KEY_UP))){}
-
+       
     if(IsKeyDown(KEY_DOWN)){}
 
-}
+
 
 //criado o arquivo dos inputs
