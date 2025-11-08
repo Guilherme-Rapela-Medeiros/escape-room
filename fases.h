6c707a2bef145fa1b6d47e729b0f2eb2c4b380b2
Guilherme-Rapela-Medeiros <guilhermerapela9@gmail.com>
2025-11-08 11:21:41 -0300

    adição de funções para inicialização e conclusão de fases

--- fases.h
+++ fases.h
@@ -1,1 +1,12 @@
+#ifndef FASES_H
+#define FASES_H
+
+#include "structs.h"
+
+void comecarfase(fases fases[], int quantidade);
+
+void acabarFases(fases fases[], int quantidade);
+
+#endif
+
 //criado o arquivo das fases
